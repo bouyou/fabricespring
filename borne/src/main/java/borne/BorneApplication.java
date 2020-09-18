962ee394151892923c6caa0444179dce507c1e47
vanoverberghe <billycobam@hotmail.fr>
2020-09-18 14:26:49 +0200

    Install +  config

---
+++ borne/src/main/java/borne/BorneApplication.java
@@ -0,0 +1,15 @@
+package borne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorneApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BorneApplication.class, args);
+		System.out.println("yo");
+	}
+
+}
